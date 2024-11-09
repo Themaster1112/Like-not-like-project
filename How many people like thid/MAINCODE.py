ef88ea95e4cb8b920bddb0637ed52b38bb796972
Pythonforbasics <114282587+Themaster1112@users.noreply.github.com>
2024-11-09 13:17:03 +0000

    first  commit

---
+++ How many people like thid/MAINCODE.py
@@ -0,0 +1,5 @@
+SURVEY=input("Did you like the school cafeteria? Say Yes or no:")
+if SURVEY == "Yes":
+    print("This student liked the school food (For teachers to see)")
+else:
+    print("This student didnt like the food")
